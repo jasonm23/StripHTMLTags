1ca51064c58c1e32775943c3fab2e4c15b102270
Jasonm23 <jasonm23@gmail.com>
2017-04-14 16:58:47 +1000

    Replace BR tags with a space

--- test/strip_tags.test.js
+++ test/strip_tags.test.js
@@ -6,5 +6,11 @@ describe('StripTags', () => {
       let html = '<p>This is simple <em>HTML</em> with <strong>nested</strong> <span class="styled with classes">tags</span></p>';
       expect(stripTags(html)).toEqual('This is simple HTML with nested tags');
     });
+
+    it('replaces br tags with space', () => {
+      let html = 'I<BR  />have<BR >some<br>line<br/>breaks<BR>you see?';
+      let expected = 'I have some line breaks you see?';
+      expect(stripTags(html)).toEqual(expected);
+    });
   });
 });
